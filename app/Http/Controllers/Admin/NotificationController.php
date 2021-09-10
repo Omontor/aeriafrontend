ff08fe9405016f91b5a0785f9d059c8304aa2141
Oliver Montor <omontor@gmail.com>
2021-09-10 13:28:44 -0500

    onesignal working dynamically for each game

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -38,12 +38,57 @@ class NotificationController extends Controller
 
     try {
 
+
+        $content = ["en" => $request->message];
+
+        $fields = [
+            'app_id' => $game->onesignal_id,
+            'included_segments' => ['All'],
+            'contents' => ['en' => $request->message],
+            'large_icon' => '',
+//            'url' => $request->link,
+            'headings' => ['en' => $request->title],
+            //'subtitle' => ['en' => $request->title]
+        ];
+
+        $fields = json_encode($fields);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8', 'Authorization: Basic ' .$game->api_key));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        $response = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        $response = json_decode($response);
+
+        if ($httpcode == 200) {
+            if (true) {
+                $request->session()->flash('status', true);
+                
         $notification = new Notification;
         $notification->title = $request->title;
         $notification->content = $request->message;
         $notification->game_id = $request->game;
         $notification->save();
         return redirect()->route('admin.notifications.index')->with('success','Notification Sent Successfuly');
+            } else {
+                dd($response);
+           return redirect()->route('admin.notifications.index')->with('error', 'OneSignal error, please try again later');
+            }
+        }
+        else
+        {
+            dd($response);
+           return redirect()->route('admin.notifications.index')->with('error', 'OneSignal error, please try again later 2'); 
+        }
+
 
     } catch (Exception $e) {
         
